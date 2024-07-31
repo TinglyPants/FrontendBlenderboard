1b0c9edb3c1062515b01dad3d475b5290aa781e3
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 20:10:57 +0100

    Err2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Logo from "./Components/NavBar/Logo";
 import NavBar from "./Components/NavBar/NavBar";
+import Profile from "./Components/NavBar/Profile";
 import SearchBar from "./Components/NavBar/SearchBar";
 
 export default function App() {
@@ -8,6 +9,13 @@ export default function App() {
             <NavBar>
                 <Logo />
                 <SearchBar />
+                <Profile
+                    username={"Cat#302241"}
+                    email={"Cat#302241@catmail.com"}
+                    image={
+                        "https://images.pexels.com/photos/1314550/pexels-photo-1314550.jpeg?auto=compress&cs=tinysrgb&w=600"
+                    }
+                />
             </NavBar>
         </div>
     );
