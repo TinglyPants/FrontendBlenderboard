05973f6e10397a3e66f00e054715c327fffec600
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 19:36:13 +0100

    Err1 Solved

--- src/Components/NavBar/Logo.js
+++ src/Components/NavBar/Logo.js
@@ -1,7 +1,7 @@
 export default function Logo() {
     return (
         <div
-            className="w-[16rem] h-full"
+            className="w-[16rem] h-full flex ml-[3rem]"
             onClick={() => console.log("Clicked Logo")}
         >
             <svg
