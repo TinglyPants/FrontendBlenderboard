8ba4240d9b23e5ef4dbf3ed87a45167fe9b15b35
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 18:40:51 +0100

    Err1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Logo from "./Components/NavBar/Logo";
+import NavBar from "./Components/NavBar/NavBar";
+
 export default function App() {
-    return <div className="w-screen h-screen bg-red-400"></div>;
+    return (
+        <div className="w-screen h-screen bg-black">
+            <NavBar>
+                <Logo />
+            </NavBar>
+        </div>
+    );
 }
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,5 @@
+export default function NavBar({ children }) {
+    return (
+        <div className="w-full h-[6rem] bg-light flex flex-row">{children}</div>
+    );
+}
