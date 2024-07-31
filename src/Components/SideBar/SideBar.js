73b50c1c35fa5949a388ea85aa2f4338bee922cf
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 20:59:47 +0100

    Sidebar.js ready

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,3 +1,3 @@
 export default function SideBar({ children }) {
-    return <div className="h-full w-[22rem] bg-red-400">{children}</div>;
+    return <div className="h-full w-[22rem] bg-mid text-white">{children}</div>;
 }
