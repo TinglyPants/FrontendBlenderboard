af3ad27cada5d6005030b991aae243ae88bd49c4
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 14:42:00 +0100

    Setup themes and fonts with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+
+            colors: {
+                dark: "#111214",
+                mid: "#17181A",
+                light: "#232426",
+                lightest: "#555555",
+                highlight: "#ff7518",
+                white: "#ffffff",
+            },
+        },
     },
     plugins: [],
 };
