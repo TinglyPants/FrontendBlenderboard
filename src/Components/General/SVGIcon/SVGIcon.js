189ce858579d5dd4aa5808286499ca9355b3e1d8
TinglyPants <chalkytangfastics@gmail.com>
2024-07-31 21:23:14 +0100

    Err solved!

--- src/Components/General/SVGIcon/SVGIcon.js
+++ src/Components/General/SVGIcon/SVGIcon.js
@@ -1,7 +1,8 @@
-export default function SVGIcon({ path, onClick, name }) {
+export default function SVGIcon({ path, onClick, name, size = "w-8 h-8" }) {
+    // Example for size prop usage: size={"w-[1.2rem] h-[2.8rem]"}
     return (
         <svg
-            className="w-8 h-8 stroke-white mx-2 group-hover:stroke-highlight hover:stroke-highlight"
+            className={`${size} stroke-white mx-2 group-hover:stroke-highlight hover:stroke-highlight`}
             viewBox="0 0 24 24"
             fill="none"
             title={name}
--- src/Components/SideBar/SideBarItem.js
+++ src/Components/SideBar/SideBarItem.js
@@ -3,7 +3,7 @@ import SVGIcon from "../General/SVGIcon/SVGIcon";
 export default function SideBarItem({ path, text, redirect }) {
     return (
         <div className="flex h-[4rem] mx-[1rem] mt-[1rem] items-center pl-[5rem]">
-            <SVGIcon path={path} name={text} />
+            <SVGIcon path={path} name={text} size={"w-[2.5rem] h-[2.5rem]"} />
             <p className="text-xl ">{text}</p>
         </div>
     );
