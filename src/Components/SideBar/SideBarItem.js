3df4a07e29d083945957d73dd2134afb64141dfd
TinglyPants <chalkytangfastics@gmail.com>
2024-08-01 16:27:11 +0100

    Added text highlighting to sidebaritem on hover

--- src/Components/SideBar/SideBarItem.js
+++ src/Components/SideBar/SideBarItem.js
@@ -2,9 +2,9 @@ import SVGIcon from "../General/SVGIcon/SVGIcon";
 
 export default function SideBarItem({ path, text, redirect }) {
     return (
-        <div className="flex h-[4rem] mx-[1rem] mt-[0.5rem] items-center pl-[4rem] cursor-pointer">
+        <div className="flex h-[4rem] mx-[1rem] mt-[0.5rem] items-center pl-[4rem] cursor-pointer group">
             <SVGIcon path={path} name={text} size={"w-[2.5rem] h-[2.5rem]"} />
-            <p className="text-xl">{text}</p>
+            <p className="text-xl group-hover:text-highlight">{text}</p>
         </div>
     );
 }
