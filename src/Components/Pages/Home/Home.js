cd664373ed3d8050af6a2cf9309e5f981fd14037
TinglyPants <chalkytangfastics@gmail.com>
2024-08-06 16:40:11 +0100

    Err9 solved

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
+    const [postIDArray, setPostIDArray] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                "http://localhost:4000/posts/homepage"
+            );
+            const received = await response.json();
+            setPostIDArray(received);
+        })();
+    }, []);
     return (
-        <div className="w-full h-full flex p-[1.5rem] bg-black overflow-y-scroll"></div>
+        <div className="w-full h-full flex flex-col p-[1.5rem] bg-black overflow-y-scroll">
+            {postIDArray.map((postID) => {
+                return <p className="text-white">{`Post ID: ${postID}`}</p>;
+            })}
+        </div>
     );
 }
