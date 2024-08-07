e9f960aa5dec03a206800623c2fa65187a437ffc
TinglyPants <chalkytangfastics@gmail.com>
2024-08-07 13:40:07 +0100

    Added suitable padding to top of sidebar, forgot to earlier

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,6 +1,6 @@
 export default function SideBar({ children }) {
     return (
-        <div className="bg-mid w-[22rem] h-full text-white border-r-2 border-lightest flex flex-col">
+        <div className="bg-mid w-[22rem] h-full pt-[4rem] text-white border-r-2 border-lightest flex flex-col">
             {children}
         </div>
     );
