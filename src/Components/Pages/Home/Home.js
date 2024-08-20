1ba674de1ba90e49ca6d6ade67640a89d4e3766e
TinglyPants <chalkytangfastics@gmail.com>
2024-08-20 12:38:11 +0100

    Homepage done, it was already pretty nice

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,19 +1,25 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "../../General/PostComponents/Post";
 import { ApiUrl } from "../../../config";
+import { ErrorContext } from "../../../App";
 
 export default function Home() {
     const [postIDArray, setPostIDArray] = useState([]);
+    const setErrorMessage = useContext(ErrorContext)[1];
 
+    // Gathering homepage posts
     useEffect(() => {
+        // useEffect requires an async IIFE as useEffect callbacks cannot be async
         (async () => {
-            try {
-                const response = await fetch(`${ApiUrl}/posts/homepage`);
-                const received = await response.json();
-                setPostIDArray(received);
-            } catch (err) {
-                console.log("Unable to contact API.");
+            const response = await fetch(`${ApiUrl}/posts/homepage`);
+
+            if (response.status !== 200) {
+                setErrorMessage(await response.text());
+                return;
             }
+
+            const received = await response.json();
+            setPostIDArray(received);
         })();
     }, []);
     return (
