c8a3c33d549348c7845e1e30cf7de1265507d435
TinglyPants <chalkytangfastics@gmail.com>
2024-08-20 17:03:02 +0100

    Added support for posts without models

--- src/Components/Pages/PostViewing/PostMediaCard.js
+++ src/Components/Pages/PostViewing/PostMediaCard.js
@@ -2,16 +2,23 @@ import MediaViewer from "../../General/MediaViewer/MediaViewer";
 import { CubeIcon } from "../../General/SVGIcon/icons";
 import SVGIcon from "../../General/SVGIcon/SVGIcon";
 import { SceneViewerContext } from "../../../App";
+import { ErrorContext } from "../../../App";
 import { useContext } from "react";
 
 export default function PostMediaCard({ videoID, imageIDs, modelID }) {
     const setSceneSettings = useContext(SceneViewerContext)[1];
+    const setErrorMessage = useContext(ErrorContext)[1];
+
     const handleOpenSceneViewer = () => {
-        setSceneSettings({
-            sceneViewerEnabled: true,
-            modelFilename: modelID,
-            modelType: modelID.split(".")[1],
-        });
+        if (modelID !== undefined) {
+            setSceneSettings({
+                sceneViewerEnabled: true,
+                modelFilename: modelID,
+                modelType: modelID.split(".")[1],
+            });
+        } else {
+            setErrorMessage("This post does not have a 3D model!");
+        }
     };
     return (
         <div className="w-full max-h-[44rem] flex flex-col ml-[1rem] bg-mid rounded-xl pb-[0.75rem]">
