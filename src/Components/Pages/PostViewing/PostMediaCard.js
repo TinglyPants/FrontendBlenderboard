b16844eb55ecf1f67ac16c75c6a8b483bba3c9ee
TinglyPants <chalkytangfastics@gmail.com>
2024-09-06 12:12:19 +0100

    Added comment creation button and context

--- src/Components/Pages/PostViewing/PostMediaCard.js
+++ src/Components/Pages/PostViewing/PostMediaCard.js
@@ -1,13 +1,17 @@
 import MediaViewer from "../../General/MediaViewer/MediaViewer";
-import { CubeIcon } from "../../General/SVGIcon/icons";
+import { CommentIcon, CubeIcon } from "../../General/SVGIcon/icons";
 import SVGIcon from "../../General/SVGIcon/SVGIcon";
 import { SceneViewerContext } from "../../../App";
 import { ErrorContext } from "../../../App";
+import { CommentCreationContext } from "./PostViewing";
 import { useContext } from "react";
 
 export default function PostMediaCard({ videoID, imageIDs, model }) {
     const setSceneSettings = useContext(SceneViewerContext)[1];
     const setErrorMessage = useContext(ErrorContext)[1];
+    const [commentCreationData, setCommentCreationData] = useContext(
+        CommentCreationContext
+    );
 
     const handleOpenSceneViewer = () => {
         if (model !== undefined) {
@@ -19,15 +23,27 @@ export default function PostMediaCard({ videoID, imageIDs, model }) {
             setErrorMessage("This post does not have a 3D model!");
         }
     };
+
+    const handleAddComment = () => {
+        setCommentCreationData({ ...commentCreationData, isEditing: true });
+    };
     return (
         <div className="w-full max-h-[44rem] flex flex-col ml-[1rem] bg-mid rounded-xl pb-[0.75rem]">
             <MediaViewer videoID={videoID} imageIDs={imageIDs} />
-            <SVGIcon
-                path={CubeIcon}
-                title="3D Viewer"
-                onClick={handleOpenSceneViewer}
-                size="w-[3rem] h-[3rem]"
-            />
+            <div className="w-full h-[3rem] flex">
+                <SVGIcon
+                    path={CubeIcon}
+                    title="3D Viewer"
+                    onClick={handleOpenSceneViewer}
+                    size="w-[3rem] h-[3rem]"
+                />
+                <SVGIcon
+                    path={CommentIcon}
+                    title="Add Comment"
+                    onClick={handleAddComment}
+                    size="w-[3rem] h-[3rem]"
+                />
+            </div>
         </div>
     );
 }
--- src/Components/Pages/PostViewing/PostViewing.js
+++ src/Components/Pages/PostViewing/PostViewing.js
@@ -1,9 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ApiUrl } from "../../../config";
 import PostInfoCard from "./PostInfoCard";
 import { ErrorContext } from "../../../App";
 import PostMediaCard from "./PostMediaCard";
+
+export const CommentCreationContext = createContext();
 
 export default function PostViewing() {
     const { id } = useParams();
@@ -22,6 +24,11 @@ export default function PostViewing() {
     const [authorData, setAuthorData] = useState({
         username: "Loading...",
         profileImage: undefined,
+    });
+
+    const [commentCreationData, setCommentCreationData] = useState({
+        isEditing: false,
+        content: "",
     });
 
     useEffect(() => {
@@ -64,22 +71,26 @@ export default function PostViewing() {
 
     return (
         <div className="w-full h-full flex flex-col p-[1.5rem] bg-black">
-            <div className="flex flex-row w-full h-full">
-                <PostInfoCard
-                    title={postData.title}
-                    description={postData.description}
-                    profileImage={authorData.profileImage}
-                    username={authorData.username}
-                    dateOfCreation={postData.dateOfCreation}
-                    author={postData.author}
-                    _id={id}
-                />
-                <PostMediaCard
-                    videoID={postData.video}
-                    imageIDs={postData.images}
-                    model={postData.model}
-                />
-            </div>
+            <CommentCreationContext.Provider
+                value={[commentCreationData, setCommentCreationData]}
+            >
+                <div className="flex flex-row w-full h-full">
+                    <PostInfoCard
+                        title={postData.title}
+                        description={postData.description}
+                        profileImage={authorData.profileImage}
+                        username={authorData.username}
+                        dateOfCreation={postData.dateOfCreation}
+                        author={postData.author}
+                        _id={id}
+                    />
+                    <PostMediaCard
+                        videoID={postData.video}
+                        imageIDs={postData.images}
+                        model={postData.model}
+                    />
+                </div>
+            </CommentCreationContext.Provider>
         </div>
     );
 }
