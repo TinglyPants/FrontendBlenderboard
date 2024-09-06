0596111e542ef04d7817742fe9c2631c8bcf7eac
TinglyPants <chalkytangfastics@gmail.com>
2024-09-06 15:51:50 +0100

    Added submit handler with no functionality

--- src/Components/Pages/PostViewing/CommentCreationTool.js
+++ src/Components/Pages/PostViewing/CommentCreationTool.js
@@ -13,14 +13,24 @@ export default function CommentCreationTool() {
         });
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return (
-        <div className="bg-mid w-full flex text-white rounded-xl p-[1rem]">
-            <form>
+        <div className="bg-mid w-full flex text-white rounded-xl p-[1rem] my-[0.65rem]">
+            <form className="w-full" onSubmit={handleSubmit}>
                 <textarea
                     placeholder="Add Comment Content"
                     className="bg-black text-white p-[0.5rem] rounded-lg w-full h-[4rem] max-h-[12rem]"
                     onChange={handleCommentContentChange}
                 ></textarea>
+                <button
+                    type="submit"
+                    className="w-[8rem] bg-highlight p-[0.5rem] rounded-full mt-[0.5rem] text-[1rem] font-semibold"
+                >
+                    Submit
+                </button>
             </form>
         </div>
     );
